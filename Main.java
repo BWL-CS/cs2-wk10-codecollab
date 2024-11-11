44a538e05fa120472a57c9f1f7025e04e386909c
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-11-11 15:19:54 +0000

    edited Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String []args) {
 
       Scanner scan = new Scanner(System.in);
+
       System.out.print("Enter your grade (9-12): ");
       int grade = scan.nextInt();
         
       System.out.print("Enter your age: ");
       int age = scan.nextInt();
-      scan.nextLine();  // why is this necessary?
+      scan.nextLine(); 
 
       System.out.print("Do you own a gaming console? (yes/no): ");
       String hasConsole = scan.nextLine();
 
       // Check eligibility based on...???
-      if (grade >= 10 && grade <= 12 && age >= 15 && age <= 18) { 
-         if (hasConsole.equalsIgnoreCase("yes")) {
+      if ((grade >= 10 && grade <= 12) && (age >= 15 && age <= 18)) { 
+         
+         if (hasConsole.equals("yes")) {
             System.out.println("You’re eligible to apply for the VIP Gaming Club!");
             
             // Follow up question
             System.out.print("Do you have at least one year of gaming experience? (yes/no): ");
             String gamingExperience = scan.nextLine();
             
-            if (gamingExperience.equalsIgnoreCase("yes")) {
+            if (gamingExperience.equals("yes")) {
                // Another follow up question
                System.out.print("Do you have a favorite multiplayer game? (yes/no): ");
                String hasFavoriteGame = scan.nextLine();
                
-               if (hasFavoriteGame.equalsIgnoreCase("yes")) {
+               if (hasFavoriteGame.equals("yes")) {
                   System.out.println("Awesome! You meet all the VIP Gaming Club criteria. Welcome aboard!");
                } 
                // Which IF statement is this ELSE associated with?
